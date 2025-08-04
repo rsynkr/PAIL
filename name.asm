6a4a536290f8d9effa8078eff14718036248ce36
rsynkr <narkarrishi16@gmail.com>
2025-08-04 22:20:28 +0530

    Add files via upload

---
+++ name.asm
@@ -0,0 +1,27 @@
+global _start 
+section .data
+	name1 db "Prathamesh" ,10
+	length1 equ $-name1
+	name2 db "Nehete" ,10
+	length2 equ $-name2
+
+section  .text 
+_start 
+	mov eax ,4
+	mov ebx ,1
+	mov ecx ,name1
+	mov edx ,length1
+	int 80h
+	
+	mov eax ,4
+	mov ebx ,1
+	mov ecx ,name2
+	mov edx ,length2
+	int 80h
+	
+	
+	xor eax,ebx
+	mov eax,1
+	int 80h
+	
+	
