56292c4506c296928838035090cca921c5da6881
rsynkr <narkarrishi16@gmail.com>
2025-08-04 22:26:46 +0530

    Add files via upload

---
+++ gdb.asm
@@ -0,0 +1,10 @@
+$ gdb ./name
+ (gdb)
+ (gdb)
+ (gdb) break _start
+ (gdb) run
+ (gdb) set disassembly-flavor intel 
+ (gdb) disassemble _start          
+ (gdb) layout asm 
+ (gdb) layout regs
+ (gdb) nexti
